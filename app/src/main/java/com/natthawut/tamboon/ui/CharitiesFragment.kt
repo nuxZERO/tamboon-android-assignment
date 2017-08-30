eb5ef9d509d3f6c068c1097522e2acf7fb4fccd3
Nux's Github <natthawut1991@gmail.com>
2017-08-30 13:21:51 +0700

    Created charities fragment.

---
+++ app/src/main/java/com/natthawut/tamboon/ui/CharitiesFragment.kt
@@ -0,0 +1,25 @@
+package com.natthawut.tamboon.ui
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.natthawut.tamboon.R
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class CharitiesFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater!!.inflate(R.layout.fragment_charities, container, false)
+    }
+
+}// Required empty public constructor
---
+++ app/src/main/java/com/natthawut/tamboon/ui/CharitiesViewModel.kt
@@ -0,0 +1,7 @@
+package com.natthawut.tamboon.ui
+
+/**
+ * Created by Nux on 8/30/2017 AD.
+ */
+class CharitiesViewModel {
+}
