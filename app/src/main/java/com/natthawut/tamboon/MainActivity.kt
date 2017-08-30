71c33e7939b1a3d587c3d233f08d36535795983d
Nux's Github <natthawut1991@gmail.com>
2017-08-30 14:44:05 +0700

    Prepaired layout to show charities list.

--- app/src/main/java/com/natthawut/tamboon/MainActivity.kt
+++ app/src/main/java/com/natthawut/tamboon/MainActivity.kt
@@ -2,11 +2,25 @@ package com.natthawut.tamboon
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import com.natthawut.tamboon.ui.CharitiesFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            initFragment(CharitiesFragment())
+        }
+
     }
+
+    private fun initFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+                .add(R.id.content_layout, fragment)
+                .commit()
+    }
+
 }
